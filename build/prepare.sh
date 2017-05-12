b7620811c4457945b48a4b2247142770a147eb68
Andy Chu <andy@oilshell.org>
2017-05-11 17:34:49 -0700

    Speed up the build by detecting the number of cores.

--- build/prepare.sh
+++ build/prepare.sh
@@ -20,13 +20,17 @@ configure() {
 # dynamically discover them with py-deps.
 #
 # Takes about 27 seconds on a fast i7 machine.
+# Ubuntu under VirtualBox on MacBook Air with 4 cores (3 jobs): 1m 25s.  The
+# Make part of the build is parallelized, but the setup.py part is not!
+
+readonly JOBS=$(( $(nproc) - 1 ))
 
 build-python() {
   cd $PY27
   make clean
   # NOTE: Python passes a few flags, so we can't use CFLAGS here.  Is would
   # have to be EXTRA_CFLAGS?
-  time make -j 7 #CFLAGS='-O0'
+  time make -j $JOBS  #CFLAGS='-O0'
   #time make -j 7 CFLAGS='-O0'
 }
 
